f424e2693ea0be66e914baa6a62f2f629ce0b453
Yangosoft <yangosoft@gmail.com>
2019-04-21 14:24:18 +0200

    Removed unused code

--- led.cpp
+++ led.cpp
@@ -27,14 +27,14 @@ extern "C"{
  */
 int _write(int file, char *ptr, int len)
 {
-	int i;
+  int i;
 
-	
-		for (i = 0; i < len; i++) {
-			
-			usart_send_blocking(USART_CONSOLE, ptr[i]);
-		}
-		return i;
+
+    for (i = 0; i < len; i++) {
+
+      usart_send_blocking(USART_CONSOLE, ptr[i]);
+    }
+    return i;
 }
 }
 
@@ -44,57 +44,57 @@ volatile uint32_t system_millis;
 /* Called when systick fires */
 void sys_tick_handler(void)
 {
-	system_millis++;
+  system_millis++;
 }
 
 /* simple sleep for delay milliseconds */
 void msleep(uint32_t delay)
 {
-	uint32_t wake = system_millis + delay;
-	while (wake > system_millis);
+  uint32_t wake = system_millis + delay;
+  while (wake > system_millis);
 }
 
 /* Getter function for the current time */
 uint32_t mtime(void)
 {
-	return system_millis;
+  return system_millis;
 }
 
 void adc_setup(void)
 {
-	gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO0);
-	gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO1);
+  gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO0);
+  gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO1);
 
-	/* Make sure the ADC doesn't run during config. */
-	adc_power_off(ADC1);
+  /* Make sure the ADC doesn't run during config. */
+  adc_power_off(ADC1);
 
-	/* We configure everything for one single conversion. */
-	adc_disable_scan_mode(ADC1);
-	adc_set_single_conversion_mode(ADC1);
-	adc_disable_external_trigger_regular(ADC1);
-	adc_set_right_aligned(ADC1);
-	adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_28DOT5CYC);
+  /* We configure everything for one single conversion. */
+  adc_disable_scan_mode(ADC1);
+  adc_set_single_conversion_mode(ADC1);
+  adc_disable_external_trigger_regular(ADC1);
+  adc_set_right_aligned(ADC1);
+  adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_28DOT5CYC);
 
-	adc_power_on(ADC1);
+  adc_power_on(ADC1);
 
-	/* Wait for ADC starting up. */
-	int i;
-	for (i = 0; i < 800000; i++) /* Wait a bit. */
-		__asm__("nop");
+  /* Wait for ADC starting up. */
+  int i;
+  for (i = 0; i < 800000; i++) /* Wait a bit. */
+    __asm__("nop");
 
-	adc_reset_calibration(ADC1);
-	adc_calibrate(ADC1);
+  adc_reset_calibration(ADC1);
+  adc_calibrate(ADC1);
 }
 
 uint16_t read_adc_naiive(uint8_t channel)
 {
-	uint8_t channel_array[16];
-	channel_array[0] = channel;
-	adc_set_regular_sequence(ADC1, 1, channel_array);
-	adc_start_conversion_direct(ADC1);
-	while (!adc_eoc(ADC1));
-	uint16_t reg16 = adc_read_regular(ADC1);
-	return reg16;
+  uint8_t channel_array[16];
+  channel_array[0] = channel;
+  adc_set_regular_sequence(ADC1, 1, channel_array);
+  adc_start_conversion_direct(ADC1);
+  while (!adc_eoc(ADC1));
+  uint16_t reg16 = adc_read_regular(ADC1);
+  return reg16;
 }
 
 
@@ -106,19 +106,19 @@ void clock_setup(void)
     rcc_periph_clock_enable(RCC_GPIOA);
     rcc_periph_clock_enable(RCC_ADC1);
     rcc_periph_clock_enable(RCC_USART1);
-    
+
    /* Base board frequency, set to 168Mhz */
 
 
-        
-        rcc_clock_setup_in_hsi_out_24mhz();
-	/* clock rate / 168000 to get 1mS interrupt rate */
-	systick_set_reload(24000);
-	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
-	systick_counter_enable();
 
-	/* this done last */
-	systick_interrupt_enable();
+        rcc_clock_setup_in_hsi_out_24mhz();
+  /* clock rate / 168000 to get 1mS interrupt rate */
+  systick_set_reload(24000);
+  systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+  systick_counter_enable();
+
+  /* this done last */
+  systick_interrupt_enable();
 
 }
 
@@ -129,19 +129,19 @@ void gpio_setup(void)
     gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL , GPIO1);
     gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_OPENDRAIN, GPIO13);
     gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_OPENDRAIN , GPIO14);
-    
-    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
-    
-    usart_set_baudrate(USART1, 38400);
-	usart_set_databits(USART1, 8);
-	usart_set_stopbits(USART1, USART_STOPBITS_1);
-	usart_set_mode(USART1, USART_MODE_TX);
-	usart_set_parity(USART1, USART_PARITY_NONE);
-	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
 
-	/* Finally enable the USART. */
-	usart_enable(USART1);
-    
+    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
+
+    usart_set_baudrate(USART1, 38400);
+  usart_set_databits(USART1, 8);
+  usart_set_stopbits(USART1, USART_STOPBITS_1);
+  usart_set_mode(USART1, USART_MODE_TX);
+  usart_set_parity(USART1, USART_PARITY_NONE);
+  usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+
+  /* Finally enable the USART. */
+  usart_enable(USART1);
+
 }
 
 void user_button_enable(void)
@@ -160,56 +160,28 @@ int main( void )
   gpio_setup();
   user_button_enable();
   adc_setup();
-  
-    
+
+
   char buffer[64];
-    
+
   std::string s;
-  
+
     while(1)
     {
-        
-       
-        usart_send_blocking(USART1, 'h');	/* USART2: Send byte. */
-        usart_send_blocking(USART1, 'e');	/* USART2: Send byte. */
-        usart_send_blocking(USART1, 'l');	/* USART2: Send byte. */
-        usart_send_blocking(USART1, 'l');	/* USART2: Send byte. */
-        usart_send_blocking(USART1, 'o');	/* USART2: Send byte. */
-        usart_send_blocking(USART1, '\n');
+
+
         gpio_toggle(GPIOB, GPIO1);
-        
+
         uint16_t adcValue = read_adc_naiive(0);
         s = std::to_string(adcValue);
-        
-        printf("%d\n",adcValue);
-        
-//         for(int i = s.length(); i > 0 ; i--)
-//         {
-//             usart_send_blocking(USART1, s[i]);    
-//         }
-//         usart_send_blocking(USART1, '\n');
-        
-        
-        msleep(1000);
-        
-        
-        
-//         printf("Toggle\n");
-//         printf("..");
-//         printf("..");
-//         printf("..");
-//         printf("..");
-//         printf("..");
-//         printf("..");
-//         printf("..");
-//         printf("..");
-//         printf("..\n");
-        
 
-           
+        printf("%d\n",adcValue);
+
+
+
+        msleep(1000);
     }
 
 
   return 0;
 }
-
