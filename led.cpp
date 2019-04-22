420c4b76fbb2e6d58de018eb5d8dc1cd99f7b39b
Yangosoft <yangosoft@gmail.com>
2019-04-22 19:51:04 +0200

    not working pwm

--- led.cpp
+++ led.cpp
@@ -101,7 +101,12 @@ void tim_setup(void)
 	 * 'output alternate function push-pull'.
 	 */
 	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
-		      GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_TIM1_CH2 );
+		      GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, 		      GPIO_TIM1_CH1 | GPIO_TIM1_CH2 | GPIO_TIM1_CH3 );
+        
+        
+        /* Enable TIM1 commutation interrupt. */
+	nvic_enable_irq(NVIC_TIM1_TRG_COM_IRQ);
+
 
 	
 
@@ -156,10 +161,250 @@ void tim_setup(void)
 
 	/* Counter enable. */
 	timer_enable_counter(TIM1);
+        
+        /* Disable outputs. */
+	timer_disable_oc_output(TIM1, TIM_OC1);
+	timer_disable_oc_output(TIM1, TIM_OC1N);
+
+	/* Configure global mode of line 1. */
+	timer_disable_oc_clear(TIM1, TIM_OC1);
+	timer_enable_oc_preload(TIM1, TIM_OC1);
+	timer_set_oc_slow_mode(TIM1, TIM_OC1);
+	timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_PWM1);
+
+	/* Configure OC1. */
+	timer_set_oc_polarity_high(TIM1, TIM_OC1);
+	timer_set_oc_idle_state_set(TIM1, TIM_OC1);
+
+	/* Configure OC1N. */
+	timer_set_oc_polarity_high(TIM1, TIM_OC1N);
+	timer_set_oc_idle_state_set(TIM1, TIM_OC1N);
+
+	/* Set the capture compare value for OC1. */
+	timer_set_oc_value(TIM1, TIM_OC1, 100);
+
+	/* Reenable outputs. */
+	timer_enable_oc_output(TIM1, TIM_OC1);
+	timer_enable_oc_output(TIM1, TIM_OC1N);
+
+	/* -- OC2 and OC2N configuration -- */
+
+	/* Disable outputs. */
+	timer_disable_oc_output(TIM1, TIM_OC2);
+	timer_disable_oc_output(TIM1, TIM_OC2N);
+
+	/* Configure global mode of line 2. */
+	timer_disable_oc_clear(TIM1, TIM_OC2);
+	timer_enable_oc_preload(TIM1, TIM_OC2);
+	timer_set_oc_slow_mode(TIM1, TIM_OC2);
+	timer_set_oc_mode(TIM1, TIM_OC2, TIM_OCM_PWM1);
+
+	/* Configure OC2. */
+	timer_set_oc_polarity_high(TIM1, TIM_OC2);
+	timer_set_oc_idle_state_set(TIM1, TIM_OC2);
+
+	/* Configure OC2N. */
+	timer_set_oc_polarity_high(TIM1, TIM_OC2N);
+	timer_set_oc_idle_state_set(TIM1, TIM_OC2N);
+
+	/* Set the capture compare value for OC1. */
+	timer_set_oc_value(TIM1, TIM_OC2, 100);
+
+	/* Reenable outputs. */
+	timer_enable_oc_output(TIM1, TIM_OC2);
+	timer_enable_oc_output(TIM1, TIM_OC2N);
+
+	/* -- OC3 and OC3N configuration -- */
+
+	/* Disable outputs. */
+	timer_disable_oc_output(TIM1, TIM_OC3);
+	timer_disable_oc_output(TIM1, TIM_OC3N);
+
+	/* Configure global mode of line 3. */
+	timer_disable_oc_clear(TIM1, TIM_OC3);
+	timer_enable_oc_preload(TIM1, TIM_OC3);
+	timer_set_oc_slow_mode(TIM1, TIM_OC3);
+	timer_set_oc_mode(TIM1, TIM_OC3, TIM_OCM_PWM1);
+
+	/* Configure OC3. */
+	timer_set_oc_polarity_high(TIM1, TIM_OC3);
+	timer_set_oc_idle_state_set(TIM1, TIM_OC3);
+
+	/* Configure OC3N. */
+	timer_set_oc_polarity_high(TIM1, TIM_OC3N);
+	timer_set_oc_idle_state_set(TIM1, TIM_OC3N);
+
+	/* Set the capture compare value for OC3. */
+	timer_set_oc_value(TIM1, TIM_OC3, 100);
+
+	/* Reenable outputs. */
+	timer_enable_oc_output(TIM1, TIM_OC3);
+	timer_enable_oc_output(TIM1, TIM_OC3N);
+
+	/* ---- */
+
+	/* ARR reload enable. */
+	timer_enable_preload(TIM1);
+
+	/*
+	 * Enable preload of complementary channel configurations and
+	 * update on COM event.
+	 */
+	timer_enable_preload_complementry_enable_bits(TIM1);
+
+	/* Enable outputs in the break subsystem. */
+	timer_enable_break_main_output(TIM1);
+
+	/* Counter enable. */
+	timer_enable_counter(TIM1);
+
+	/* Enable commutation interrupt. */
+	timer_enable_irq(TIM1, TIM_DIER_COMIE);
 
 	
 }
 
+extern "C"{
+void tim1_trg_com_isr(void)
+{
+	static int step = 0;
+
+	/* Clear the COM trigger interrupt flag. */
+	timer_clear_flag(TIM1, TIM_SR_COMIF);
+
+	/*
+	 * A simplified and inefficient implementation of PWM On
+	 * scheme. Look at the implementation in Open-BLDC on
+	 * http://open-bldc.org for the proper implementation. This
+	 * one only serves as an example.
+	 *
+	 * Table of the PWM scheme zone configurations when driving:
+	 * @verbatim
+	 *  | 1| 2| 3| 4| 5| 6|
+	 * -+--+--+--+--+--+--+
+	 * A|p+|++|  |p-|--|  |
+	 * -+--+--+--+--+--+--+
+	 * B|  |p-|--|  |p+|++|
+	 * -+--+--+--+--+--+--+
+	 * C|--|  |p+|++|  |p-|
+	 * -+--+--+--+--+--+--+
+	 *  |  |  |  |  |  |  '- 360 Deg
+	 *  |  |  |  |  |  '---- 300 Deg
+	 *  |  |  |  |  '------- 240 Deg
+	 *  |  |  |  '---------- 180 Deg
+	 *  |  |  '------------- 120 Deg
+	 *  |  '----------------  60 Deg
+	 *  '-------------------   0 Deg
+	 *
+	 * Legend:
+	 * p+: PWM on the high side
+	 * p-: PWM on the low side
+	 * --: Low side on
+	 * ++: High side on
+	 *   : Floating/NC
+	 * @endverbatim
+	 */
+	switch (step) {
+	case 0: /* A PWM HIGH, B OFF, C LOW */
+		timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_PWM1);
+		timer_set_oc_mode(TIM1, TIM_OC2, TIM_OCM_FROZEN);
+		timer_set_oc_mode(TIM1, TIM_OC3, TIM_OCM_FORCE_LOW);
+
+		timer_enable_oc_output(TIM1, TIM_OC1);
+		timer_disable_oc_output(TIM1, TIM_OC1N);
+
+		timer_disable_oc_output(TIM1, TIM_OC2);
+		timer_disable_oc_output(TIM1, TIM_OC2N);
+
+		timer_enable_oc_output(TIM1, TIM_OC3);
+		timer_enable_oc_output(TIM1, TIM_OC3N);
+
+		step++;
+		break;
+	case 1: /* A HIGH, B PWM LOW, C OFF */
+		timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_FORCE_HIGH);
+		timer_set_oc_mode(TIM1, TIM_OC2, TIM_OCM_PWM1);
+		timer_set_oc_mode(TIM1, TIM_OC3, TIM_OCM_FROZEN);
+
+		timer_enable_oc_output(TIM1, TIM_OC1);
+		timer_enable_oc_output(TIM1, TIM_OC1N);
+
+		timer_disable_oc_output(TIM1, TIM_OC2);
+		timer_enable_oc_output(TIM1, TIM_OC2N);
+
+		timer_disable_oc_output(TIM1, TIM_OC3);
+		timer_disable_oc_output(TIM1, TIM_OC3N);
+
+		step++;
+		break;
+	case 2: /* A OFF, B LOW, C PWM HIGH */
+		timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_FROZEN);
+		timer_set_oc_mode(TIM1, TIM_OC2, TIM_OCM_FORCE_LOW);
+		timer_set_oc_mode(TIM1, TIM_OC3, TIM_OCM_PWM1);
+
+		timer_disable_oc_output(TIM1, TIM_OC1);
+		timer_disable_oc_output(TIM1, TIM_OC1N);
+
+		timer_enable_oc_output(TIM1, TIM_OC2);
+		timer_enable_oc_output(TIM1, TIM_OC2N);
+
+		timer_enable_oc_output(TIM1, TIM_OC3);
+		timer_disable_oc_output(TIM1, TIM_OC3N);
+
+		step++;
+		break;
+	case 3: /* A PWM LOW, B OFF, C HIGH */
+		timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_PWM1);
+		timer_set_oc_mode(TIM1, TIM_OC2, TIM_OCM_FROZEN);
+		timer_set_oc_mode(TIM1, TIM_OC3, TIM_OCM_FORCE_HIGH);
+
+		timer_disable_oc_output(TIM1, TIM_OC1);
+		timer_enable_oc_output(TIM1, TIM_OC1N);
+
+		timer_disable_oc_output(TIM1, TIM_OC2);
+		timer_disable_oc_output(TIM1, TIM_OC2N);
+
+		timer_enable_oc_output(TIM1, TIM_OC3);
+		timer_enable_oc_output(TIM1, TIM_OC3N);
+
+		step++;
+		break;
+	case 4: /* A LOW, B PWM HIGH, C OFF */
+		timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_FORCE_LOW);
+		timer_set_oc_mode(TIM1, TIM_OC2, TIM_OCM_PWM1);
+		timer_set_oc_mode(TIM1, TIM_OC3, TIM_OCM_FROZEN);
+
+		timer_enable_oc_output(TIM1, TIM_OC1);
+		timer_enable_oc_output(TIM1, TIM_OC1N);
+
+		timer_enable_oc_output(TIM1, TIM_OC2);
+		timer_disable_oc_output(TIM1, TIM_OC2N);
+
+		timer_disable_oc_output(TIM1, TIM_OC3);
+		timer_disable_oc_output(TIM1, TIM_OC3N);
+
+		step++;
+		break;
+	case 5: /* A OFF, B HIGH, C PWM LOW */
+		timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_FROZEN);
+		timer_set_oc_mode(TIM1, TIM_OC2, TIM_OCM_FORCE_HIGH);
+		timer_set_oc_mode(TIM1, TIM_OC3, TIM_OCM_PWM1);
+
+		timer_disable_oc_output(TIM1, TIM_OC1);
+		timer_disable_oc_output(TIM1, TIM_OC1N);
+
+		timer_enable_oc_output(TIM1, TIM_OC2);
+		timer_enable_oc_output(TIM1, TIM_OC2N);
+
+		timer_disable_oc_output(TIM1, TIM_OC3);
+		timer_enable_oc_output(TIM1, TIM_OC3N);
+
+		step = 0;
+		break;
+	}
+
+}
+}
 
 void adc_setup(void)
 {
@@ -264,6 +509,19 @@ int main( void )
   user_button_enable();
   adc_setup();
   tim_setup();
+  
+  timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_PWM1);
+		timer_set_oc_mode(TIM1, TIM_OC2, TIM_OCM_FROZEN);
+		timer_set_oc_mode(TIM1, TIM_OC3, TIM_OCM_FORCE_LOW);
+
+		timer_enable_oc_output(TIM1, TIM_OC1);
+		timer_disable_oc_output(TIM1, TIM_OC1N);
+
+		timer_disable_oc_output(TIM1, TIM_OC2);
+		timer_disable_oc_output(TIM1, TIM_OC2N);
+
+		timer_enable_oc_output(TIM1, TIM_OC3);
+		timer_enable_oc_output(TIM1, TIM_OC3N);
 
   char buffer[64];
 
